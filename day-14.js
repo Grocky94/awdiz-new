76925a626419e9d2a4c03a96665d0ad90c8db107
Grocky94 <lastleftdefence@gmail.com>
2023-05-27 13:44:17 +0530

    javascript start

---
+++ day-14.js
@@ -0,0 +1,27 @@
+//  earth - solid , liquid , gas 
+
+
+//  javaScript - dataTypes 
+// 1)strings - "" , '' , `` 
+// 2) numbers - 0123 , 245 , 567 ,8910
+// 3) Boolean - true , false
+
+// variable - it is something which can store data like a box 
+
+// it's syntax 
+
+// var kuchbhi = 'Rocky';
+// console.log(kuchbhi);
+
+// var kuchbhi = '123456';
+// console.log(kuchbhi);
+// console.log(typeof(kuchbhi));
+// var kuchbhi = 123456789;
+// console.log(kuchbhi);
+// console.log(typeof(kuchbhi));
+
+// var person = "true";
+// console.log("data-",person);
+// console.log("dataType-",typeof(person));
+
+
