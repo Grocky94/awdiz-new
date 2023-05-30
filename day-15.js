bdfa59a3c90efa73058ebf76e7a86239a4490f12
Grocky94 <lastleftdefence@gmail.com>
2023-05-30 14:26:14 +0530

    awdiz teaching update

---
+++ day-15.js
@@ -0,0 +1,66 @@
+// // arthematic opration 
+// var number1 = 10;
+// var number2 = 20;
+
+// var addition = number1 + number2;
+// console.log(addition);
+// var substraction = number2 - number1;
+// console.log(substraction);
+// var multiplication = number1 * number2;
+// console.log(multiplication);
+// var divide = number2 / number1;
+// console.log(divide);
+
+// // conditional statement 
+// // 1) if else 
+// // 2) switch
+
+// // if else 
+
+// // if (conditions){
+// //     code to be execute
+// //     console.log("condition true")
+// // } else if (condition second){
+// //     code to be execute
+// //     console.log ("condition true")
+// // } else {
+// //     code to be excute 
+// //     console.log ("condition false")
+// // }
+
+// // double operator 
+
+// var num1 = 123;
+// var num2 = "123";
+
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+
+// var num = 98;
+// console.log(num1 > 50 && num1 < 99 && num1 < 10)
+// // and oprator excecute when all have to be true.
+
+// var num1 = 98;
+// console.log(num1 > 50 || num1 < 90 || num1 < 20)
+// // or operator excecute even if one condition get right.
+
+// var userAge = 25;
+
+// if (userAge == 18) {
+//     console.log("You are applicable for Learning Licence...")
+// } else if( userAge < 80 && userAge > 18 ){
+//     console.log("You are allow for Driving Licence..")
+// } else {
+//     console.log("You are NOT allow for driving licenceee...")
+// }
+
+var i = 0;
+console.log(i);
+i++;
+console.log(i);
+console.log(i+10);
+i++;
+i++;
+console.log(i);
+
---
+++ day-16.js
@@ -0,0 +1,23 @@
+// var person = ['rocky', 'santhosh','ashok','manoj', 'rajveer' ]
+
+// var i;
+// for(i=0; i <= person.length; i = i + 1){
+//     console.log(person[i]);
+// }
+
+// if (){}
+// for (initial condition, ending condition; step)
+
+// for (var i = 1; i<= 100 ; i++){
+//     console.log("hello", i);
+// }
+
+for (var x = 3; x <= 5000 ; x = x ** 3 ){
+    console.log ("mr.x", x);
+}
+
+// for (var i = 5; i <= 1000; i = i ** 2) {
+//     console.log("Hello", i);
+// }
+
+for (var i )
