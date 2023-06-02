f93e2f95929d1baa051097f956812d846c846556
Grocky94 <lastleftdefence@gmail.com>
2023-06-02 22:21:24 +0530

    update

---
+++ day-18-nested-loops.js
@@ -0,0 +1,34 @@
+// nested loops 
+// for (initial , condition , increment;) {
+//  for (second initial, second condition, second increment){
+
+//  }
+// }
+
+
+// looping inside loop and comparing 
+
+// => using if and else statement 
+
+// Q. given an target find two from array which is addition is target. 
+
+var target = 93;
+// var array = [3, 54, 32, 6, 23, 56, 76, 17];
+// // result = 76 17
+
+// for (var i = 0; i < array.length; i++) {
+//     // console.log(array[i]);
+//     for (j = i + 1; j < array.length; j++) {
+//         // console.log(array[i], array[j]);
+//         if (array[i] + array[j] == target) {
+//             console.log(array[i], array[j]);
+//         }
+//     }
+// }
+
+var sum = 23;
+var numbers = [46,30,17, 25, 28, 2,23, 5];
+
+for (var i = 0; i > numbers.length ; i = i - 1){
+    console.log(number[i]);
+}
